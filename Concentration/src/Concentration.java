07a6b64c9973583123b74a4f1e4d90a48c580c42
Ethan.Walters <Ethan.Walters@mu-c1-03.mullen.pvt.k12.co.us>
2017-09-14 14:57:54 -0600

    initial commit

---
+++ Concentration/src/Concentration.java
@@ -0,0 +1,21 @@
+
+public class Concentration
+	{
+
+		public static void main(String[] args)
+			{
+				// creating the what the user will see
+				
+				String [] [] concentrationGame = new String [4][4];
+				
+				concentrationGame [0][0] = "pipes";
+				concentrationGame [0][1] = "metallic";
+				concentrationGame [0][2] = "gloomy";
+				concentrationGame [0][3] = "half";
+				concentrationGame [1][0] = "discovery";
+				concentrationGame [1][1] = "gadget";
+				concentrationGame [][] = "";
+
+			}
+
+	}
